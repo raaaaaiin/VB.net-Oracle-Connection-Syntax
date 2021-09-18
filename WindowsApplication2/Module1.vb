42bdf229891e058e695eb49626926b8639888640
raaaaaiin <marcraineer0089@gmail.com>
2021-09-19 07:03:25 +0800

    Initial Commit

---
+++ WindowsApplication2/Module1.vb
@@ -0,0 +1,18 @@
+﻿Imports Oracle.DataAccess.Client
+
+
+Module Module1
+    Public ORACLECON As OracleConnection
+    Public OracleCom As OracleCommand
+    Public OracleRead As OracleDataReader
+    Public Oracleadpt As OracleDataAdapter
+    Public DS As New DataSet
+    Public myData As New DataTable
+    Public Sub Connection()
+        ORACLECON = New OracleConnection("Data Source=(DESCRIPTION=" _
+           + "(ADDRESS=(PROTOCOL=TCP) (HOST=127.0.0.1) (PORT=8080))" _
+           + "(CONNECT_DATA=(SERVICE_NAME=ORCL)));" _
+           + "User Id=hr;Password=hr;")
+        ORACLECON.Open()
+    End Sub
+End Module
